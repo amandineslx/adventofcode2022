b8481333519c88d51d0e05a0b49ebd7df69a02fe
Amandine Souilleux <amandine.souilleux@dataiku.com>
2022-12-27 00:30:39 +0100

    Day 11 part 2

--- 11/11.py
+++ 11/11.py
@@ -1,5 +1,5 @@
 INPUT_FILE = '11-input.txt'
-MAX_NUMBER_OF_ROUNDS = 20
+MAX_NUMBER_OF_ROUNDS = 10000
 
 class Monkey:
     def __init__(self, number):
@@ -14,10 +14,10 @@ class Monkey:
     def to_string(self):
         return f'Monkey {self.number}: activity={self.activity}, items=({self.items}), operation={self.operation}, divisibility_test={self.divisibility_test}, monkey_if_true={self.monkey_number_if_true}, monkey_if_false={self.monkey_number_if_false}'
 
-    def inspect_item(self, item):
+    def inspect_item(self, item, global_modulo=None):
         self.activity += 1
         new_item_value = worry_about_item(item, self.operation)
-        new_item_value = feel_relief(new_item_value)
+        new_item_value = feel_relief(new_item_value, global_modulo)
         return new_item_value, self.get_monkey_target_number(new_item_value)
 
     def get_monkey_target_number(self, item):
@@ -36,8 +36,11 @@ def worry_about_item(item, operation):
     old = item
     return int(eval(operation))
 
-def feel_relief(item):
-    return item // 3
+def feel_relief(item, global_modulo=None):
+    if not global_modulo:
+        return item // 3
+    else:
+        return item % global_modulo
 
 def initialize_monkeys():
     monkeys = []
@@ -70,12 +73,15 @@ def print_monkeys(monkeys):
         print(monkey.to_string())
 
 def take_rounds(monkeys):
+    global_modulo = 1
+    for monkey in monkeys:
+        global_modulo *= monkey.divisibility_test
     for i in range(MAX_NUMBER_OF_ROUNDS):
         print(f"ROUND {i}")
         for monkey in monkeys:
             for item in monkey.items:
                 #print(f"Monkey={monkey.number}, item={item}")
-                throw = monkey.inspect_item(item)
+                throw = monkey.inspect_item(item, global_modulo)
                 monkey.throw_item(throw[0], monkeys[throw[1]])
             monkey.finish_round()
         print_monkeys(monkeys)
