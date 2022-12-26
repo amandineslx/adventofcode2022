930790a89ae912fdaa1ed80b3cdea343cf203372
Amandine Souilleux <amandine.souilleux@dataiku.com>
2022-12-26 14:28:55 +0100

    Day 10 part 2

--- 10/10.py
+++ 10/10.py
@@ -1,13 +1,22 @@
-INPUT_FILE = "10-input-test-2.txt"
+INPUT_FILE = "10-input.txt"
+CYCLES_PADDING = 40
+FIRST_CYCLE = 20
+LAST_CYCLE = 220
 
 class Machine:
     def __init__(self):
         self.tick = 1
         self.register = 1
         self.cumulated_signal_strengths = 0
+        number_of_lines = ((LAST_CYCLE - FIRST_CYCLE) // CYCLES_PADDING) + 1
+        self.screen = [[] for _ in range(number_of_lines)]
 
     def to_string(self):
-        return f"Machine: tick={self.tick}, register={self.register}, signal_strength={self.cumulated_signal_strengths}"
+        result = f"Machine: tick={self.tick}, register={self.register}, signal_strength={self.cumulated_signal_strengths}"
+        for i in range(len(self.screen)):
+            result += '\n'
+            result += ''.join(self.screen[i])
+        return result
 
     def perform_instruction(self, line):
         #print(line)
@@ -18,12 +27,14 @@ class Machine:
             self.addx(int(line_parts[1]))
 
     def noop(self):
+        self.add_pixel()
         self.tick += 1
         if self.on_signal_strength_tick():
             self.add_to_signal_strength()
 
     def addx(self, x):
         self.noop()
+        self.add_pixel()
         self.tick += 1
         self.register += x
         if self.on_signal_strength_tick():
@@ -33,10 +44,23 @@ class Machine:
         return self.tick * self.register
 
     def on_signal_strength_tick(self):
-        return self.tick <= 220 and self.tick % 40 == 20
+        return self.tick <= LAST_CYCLE and self.tick % CYCLES_PADDING == FIRST_CYCLE
 
     def add_to_signal_strength(self):
         self.cumulated_signal_strengths += self.get_signal_strength()
+        print(self.to_string())
+
+    def add_pixel(self):
+        line = (self.tick - 1) // CYCLES_PADDING
+        col = (self.tick - 1) % CYCLES_PADDING
+        print(f"Pixel position: ({line}, {col})")
+        if col in [self.register - 1, self.register, self.register + 1]:
+            self.screen[line].append('#')
+        else:
+            self.screen[line].append('.')
+        if len(self.screen[line]) != col + 1:
+            print(self.to_string())
+            raise Exception(f"Misalignment between line length {len(self.screen[line])} and column number {col + 1}")
         print(self.to_string())
 
 def perform_instructions():
