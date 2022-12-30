1db99f90c3a8dfa4a3e2464cbed89dbca000edf0
Amandine Souilleux <amandine.souilleux@dataiku.com>
2022-12-30 23:23:12 +0100

    Day 13 part 2 working with test data

--- 13/13.py
+++ 13/13.py
@@ -1,55 +1,64 @@
 from collections.abc import Iterable
+from functools import cmp_to_key
 
 INPUT_FILE = '13-input-test.txt'
+SEPARATOR1 = [[2]]
+SEPARATOR2 = [[6]]
 
 def compare(a, b):
-    #print(a)
-    #print(b)
-    result = True
+    print(a)
+    print(b)
     if isinstance(a, int) and isinstance(b, int):
-        if a < b:
-            return True
-        if a == b:
-            return None
-        if a > b:
-            return False
-        return a <= b
+        return (a > b) - (a < b)
     elif isinstance(a, Iterable) and isinstance(b, Iterable):
-        min_common_length = min(len(a), len(b))
-        if min_common_length == 0:
-            return compare(len(a), len(b))
-        for i in range(min_common_length):
-            comparison = compare(a[i], b[i])
-            if comparison == False:
-                return False
-            elif comparison == True:
-                return True
-            elif i == min_common_length - 1 and comparison == None:
-                return compare(len(a), len(b))
+        if len(a) and len(b):
+            comparison = compare(a[0], b[0])
+            if comparison != 0:
+                return comparison
+            else:
+                return compare(a[1:], b[1:])
+        return compare(len(a), len(b))
     else:
         if isinstance(a, int):
             a = [a]
-        else:
+        if isinstance(b, int):
             b = [b]
-        result &= compare(a, b)
-    #print(f"Comparison result: {result}")
-    return result
+        return compare(a, b)
 
 def check_packets_order():
     ordered_packet_ids = []
     with open(INPUT_FILE) as f:
         lines = f.readlines()
-        for i in range(len(lines) // 3):
+        for i in range((len(lines) // 3) + 1):
             #print(f"========== ID {i+1} ==========")
             packet1 = eval(lines[i*3][:-1])
             packet2 = eval(lines[i*3+1][:-1])
-            if compare(packet1, packet2):
+            if compare(packet1, packet2) <= 0:
                 ordered_packet_ids.append(i+1)
     return ordered_packet_ids
 
 def sum_packet_ids(ordered_packet_ids):
     return sum(ordered_packet_ids)
 
-ordered_packet_ids = check_packets_order()
-print(ordered_packet_ids)
-print(sum_packet_ids(ordered_packet_ids))
+def order_packets():
+    packets = []
+    with open(INPUT_FILE) as f:
+        lines = f.readlines()
+        for i in range((len(lines) // 3) + 1):
+            packets.append(eval(lines[i*3][:-1]))
+            packets.append(eval(lines[i*3+1][:-1]))
+    packets.append(SEPARATOR1)
+    packets.append(SEPARATOR2)
+    packets = sorted(packets, key=cmp_to_key(compare))
+    return packets
+
+def find_decoder_key(packets):
+    index_separator1 = packets.index(SEPARATOR1) + 1
+    index_separator2 = packets.index(SEPARATOR2) + 1
+    return index_separator1 * index_separator2
+
+#ordered_packet_ids = check_packets_order()
+#print(ordered_packet_ids)
+#print(sum_packet_ids(ordered_packet_ids))
+ordered_packets = order_packets()
+print(find_decoder_key(ordered_packets))
